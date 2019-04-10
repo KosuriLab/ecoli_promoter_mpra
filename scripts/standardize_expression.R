0ad5c9544de75b030513d6a6885879e7fcc83d96
kinsigne <kiminsigne@gmail.com>
2019-04-10 13:25:12 -0700

    standardized peak tile expression

--- scripts/standardize_expression.R
+++ scripts/standardize_expression.R
@@ -9,8 +9,6 @@ options(stringsAsFactors = F)
 
 library(dplyr)
 library(tidyr)
-library(ggplot2)
-library(cowplot)
 
 
 tss <- read.table('../processed_data/endo_tss/lb/rLP5_Endo2_lb_expression_formatted.txt', 
@@ -18,6 +16,9 @@ tss <- read.table('../processed_data/endo_tss/lb/rLP5_Endo2_lb_expression_format
 
 scramble <- read.table('../processed_data/endo_scramble/endo_scramble_expression_formatted.txt', 
                        header = T)
+
+peak_tile <- read.table('../processed_data/peak_tile/peak_tile_expression_formatted.txt',
+                        header = T)
 
 wt_scramble <- scramble %>% 
     filter(category == 'unscrambled') %>% 
@@ -29,8 +30,8 @@ wt_robust_fit <- MASS::rlm(expn_med_tss ~ expn_med_scramble, wt_scramble)
 # fit expression using robust fit, rename column so it works with fit
 scramble$expn_med_fitted <- predict(wt_robust_fit, 
                                     select(scramble, expn_med_scramble = expn_med))
-# peak_tile$expn_med_fitted <- predict(wt_robust_fit, 
-#                                      select(peak_tile, expn_med_scramble = expn_med))
+peak_tile$expn_med_fitted <- predict(wt_robust_fit,
+                                     select(peak_tile, expn_med_scramble = expn_med))
 
 # we do not need to scale TSS because it's the standard, but create a "fitted"
 # column for consistency
@@ -45,20 +46,16 @@ set_std_threshold <- function(df) {
     return(df$expn_med_fitted * scale)
 }
 
-wt_scramble <- scramble %>% 
-    filter(category == 'unscrambled') %>% 
-    left_join(select(tss, tss_name, RNA_exp_sum_ave, expn_med_fitted_scaled), by = 'tss_name',
-              suffix = c('_scramble', '_tss'))
-
 tss$expn_med_fitted_scaled <- set_std_threshold(tss)
 scramble$expn_med_fitted_scaled <- set_std_threshold(scramble)
+peak_tile$expn_med_fitted_scaled <- set_std_threshold(peak_tile)
 
 # now that expression is standardized, calculate change in expression for scramble
 # calculate change in expression: unscramble - scramble
 scramble <- scramble %>% 
     group_by(tss_name) %>% 
-    mutate(unscrambled_exp = ifelse(any(category == 'unscramble'),
-                                    RNA_exp_sum_ave[category == 'unscramble'],
+    mutate(unscrambled_exp = ifelse(any(category == 'unscrambled'),
+                                    RNA_exp_sum_ave[category == 'unscrambled'],
                                     NA),
            relative_exp = RNA_exp_sum_ave / unscrambled_exp)
 
@@ -66,5 +63,6 @@ write.table(tss, file = '../processed_data/endo_tss/lb/rLP5_Endo2_lb_expression_
             row.names = F, quote = F, sep = '\t')
 write.table(scramble, file = '../processed_data/endo_scramble/endo_scramble_expression_formatted_std.txt',
             row.names = F, quote = F, sep = '\t')
-
+write.table(scramble, file = '../processed_data/peak_tile/peak_tile_expression_formatted_std.txt',
+            row.names = F, quote = F, sep = '\t')
 
