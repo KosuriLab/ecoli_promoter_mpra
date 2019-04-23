e1bd0e33e7ef8edb376ebd752a2a93fd3978f636
Kimberly <kiminsigne@gmail.com>
2019-04-22 21:10:13 -0700

    fixed variable in output

--- scripts/peak_tile/run_nn_peak_tile_regression.sh
+++ scripts/peak_tile/run_nn_peak_tile_regression.sh
@@ -6,4 +6,4 @@ echo "Running hyperparameter tuning for regression..."
 python ../nn/dragonn_hyperparameter_tuning_regression.py \
 $DATA/peak_tile_expression_90train.txt \
 $DATA/peak_tile_expression_10test.txt \
-150 4 5 100 0.2 100 $DATA/$DATE_peak_tile_hyperparam_tuned > $DATA/$DATE_tune_log.txt
+150 4 5 100 0.2 100 $DATA/${DATE}_peak_tile_hyperparam_tuned > $DATA/${DATE}_hyperparam_tuned.log
