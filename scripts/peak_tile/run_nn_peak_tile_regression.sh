c5118c0b2897c83a4883d330383f794f7bb08b1a
kinsigne <kiminsigne@gmail.com>
2019-04-23 06:27:11 -0700

    nn tweaks

--- scripts/peak_tile/run_nn_peak_tile_regression.sh
+++ scripts/peak_tile/run_nn_peak_tile_regression.sh
@@ -3,7 +3,8 @@ DATE=`date +%Y%m%d`
 
 echo "Running hyperparameter tuning for regression..."
 
+# touch ${DATA}/${DATE}_peak_tile_hyperparam_tuned.log
 python ../nn/dragonn_hyperparameter_tuning_regression.py \
 $DATA/peak_tile_expression_90train.txt \
 $DATA/peak_tile_expression_10test.txt \
-150 4 5 100 0.2 100 $DATA/${DATE}_peak_tile_hyperparam_tuned > $DATA/${DATE}_hyperparam_tuned.log
+150 4 5 100 0.2 100 $DATA/${DATE}_peak_tile_hyperparam_tuned > $DATA/${DATE}_peak_tile_hyperparam_tuned.log
