d45642c3eec19a4eaa91dff9c8867f1c3374e9cb
kinsigne <kiminsigne@gmail.com>
2019-04-23 07:37:52 -0700

    classification script

--- scripts/nn/dragonn_hyperparameter_tuning.py
+++ scripts/nn/dragonn_hyperparameter_tuning.py
@@ -71,50 +71,68 @@ def encode_trim_pad_fasta_sequences(fname, max_length):
     return one_hot_encode(np.array(sequences))
 
 
+def process_seqs(filename, seq_length, activity_type):
+
+	X = encode_trim_pad_fasta_sequences(filename, seq_length)
+	if activity_type == 'active':
+		y = np.array([[True]]*len(X))
+	elif activity_type == 'inactive':
+		y = np.array([[False]]*len(X))
+	else:
+		raise ValueException('Please specify activity type: active or inactive')
+
+	return [X, y]
+
+
 num_epochs = 100
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('positives', help='fasta file of positive sequences')
-	parser.add_argument('negatives', help='fasta file of negative sequences')
+	parser.add_argument('pos_train', help='fasta file of positive sequences, train')
+	parser.add_argument('neg_train', help='fasta file of negative sequences, train')
+	parser.add_argument('pos_test', help='fasta file of positive sequences, test')
+	parser.add_argument('neg_test', help='fasta file of negative sequences, test')
+
 	parser.add_argument('seq_length', type=int, help='length of input sequences, or max length for trimming/padding sequences')
 	parser.add_argument('num_layers', type=int, help='number of convolutional layers')
 	parser.add_argument('min_filter', type=int, help='minimum number of filters')
 	parser.add_argument('max_filter', type=int, help='maximum number of filters')
-	parser.add_argument('test_fraction', type=float)
 	parser.add_argument('validation_fraction', type=float)
 	parser.add_argument('num_trials', type=int, 
 		help='number of hyperparameter trials')
+	parser.add_argument('prefix', help='output prefix for saved model files')
 	args = parser.parse_args()
 
-	pos_sequences = args.positives
-	neg_sequences = args.negatives
+	pos_train = args.pos_train
+	neg_train = args.neg_train
+	pos_test = args.pos_test
+	neg_test = args.neg_test
+
 	seq_length = args.seq_length
 	num_layers = args.num_layers
 	min_filter = args.min_filter
 	max_filter = args.max_filter
-	test_fraction = args.test_fraction
 	validation_fraction = args.validation_fraction
 	num_hyperparameter_trials = args.num_trials
+	prefix = args.prefix
 
 	# read in sequences and labels
 	print("loading sequence data...")
-	X_pos = encode_trim_pad_fasta_sequences(pos_sequences, seq_length)
-	y_pos = np.array([[True]]*len(X_pos))
-	X_neg = encode_trim_pad_fasta_sequences(neg_sequences, seq_length)
-	y_neg = np.array([[False]]*len(X_neg))
-	X = np.concatenate((X_pos, X_neg))
-	y = np.concatenate((y_pos, y_neg))
 
-	print('Partitioning data into training, validation and test sets...')
-	X_train, X_test, y_train, y_test = train_test_split(X, y, 
-		test_size=test_fraction)
+	X_pos, y_pos = process_seqs(args.pos_train, seq_length, 'active')
+	X_neg, y_neg = process_seqs(args.neg_train, seq_length, 'inactive')
+	X_train = np.concatenate((X_pos, X_neg))
+	y_train = np.concatenate((y_pos, y_neg))
+
 	X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, 
 		test_size=validation_fraction)
 
-
-
+	X_pos_test, y_pos_test = process_seqs(args.pos_test, seq_length, 'active')
+	X_neg_test, y_neg_test = process_seqs(args.neg_test, seq_length, 'inactive')
+	X_test = np.concatenate((X_pos_test, X_neg_test))
+	y_test = np.concatenate((y_pos_test, y_neg_test))
+	
 	print('Starting hyperparameter search...')
 
 	min_conv_width = 15
@@ -138,11 +156,30 @@ if __name__ == '__main__':
 	searcher = HyperparameterSearcher(SequenceDNN, fixed_hyperparameters, grid, 
 		X_train, y_train, validation_data=(X_valid, y_valid), backend=RandomSearch)
 	searcher.search(num_hyperparameter_trials)
+	
 	print('Best hyperparameters: {}'.format(searcher.best_hyperparameters))
 	model = searcher.best_model
-	# Test model
+	
+	# print test results
 	print('Test results: {}'.format(model.test(X_test, y_test)))
+	
+	# save model
+	model.save(prefix)
 
-	model.save('hyperparam_tuned_model')
+	# print predictions
+	predictions = model.predict(X_test)
+
+	# extract sequences for prediction file
+	test_sequences = np.concatenate((
+		encode_trim_pad_fasta_sequences(args.pos_test, 150),
+		encode_trim_pad_fasta_sequences(args.neg_test, 150)))
+
+	
+	with open(prefix + '_predictions.txt', 'w') as outfile:
+		for i in range(len(predictions)):
+			outfile.write(
+				test_sequences[i] + '\t' + 
+				str(float(predictions[i])) + '\t' + 
+				str(float(y_test[i])) + '\n')
 
 
