2a6ceecabd1c8a6138d8b3505d6cd679e9caa781
kinsigne <kiminsigne@gmail.com>
2019-04-29 15:54:03 -0700

    scramble nn script

---
+++ scripts/endo_scramble/run_nn_scramble_regression.sh
@@ -0,0 +1,11 @@
+TRAIN=$1
+TEST=$2
+OUTPUT=$3
+DATE=`date +%Y%m%d`
+
+echo "Running hyperparameter tuning for regression..."
+
+# touch ${DATA}/${DATE}_peak_tile_hyperparam_tuned.log
+python ../nn/dragonn_hyperparameter_tuning_regression.py \
+$TRAIN $TEST
+150 4 5 100 0.2 100 $OUTPUT/${DATE}_scramble_hyperparam_tuned > $OUTPUT/${DATE}_scramble_hyperparam_tuned.log
