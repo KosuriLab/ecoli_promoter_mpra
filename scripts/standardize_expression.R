d60eb23d8b196c8d2fbd0cbe7c55208b1fea6005
kinsigne <kiminsigne@gmail.com>
2019-05-09 12:02:49 -0700

    tweaked standardization to use common positive controls with TSS for each library individually

--- scripts/standardize_expression.R
+++ scripts/standardize_expression.R
@@ -26,22 +26,39 @@ flp3 <- read.table('../processed_data/endo_tss/alt_landing_pads/fLP3/fLP3_Endo2_
 rlp6 <- read.table('../processed_data/endo_tss/alt_landing_pads/rLP6/rLP6_Endo2_lb_expression_formatted.txt',
                    header = T)
 
-wt_scramble <- scramble %>% 
-    filter(category == 'unscrambled') %>% 
-    left_join(select(tss, tss_name, RNA_exp_sum_ave, expn_med), by = 'tss_name',
-              suffix = c('_scramble', '_tss'))
+# wt_scramble <- scramble %>% 
+#     filter(category == 'unscrambled') %>% 
+#     left_join(select(tss, tss_name, RNA_exp_sum_ave, expn_med), by = 'tss_name',
+#               suffix = c('_scramble', '_tss'))
+# 
+# wt_robust_fit <- MASS::rlm(expn_med_tss ~ expn_med_scramble, wt_scramble)
+# 
+# # fit expression using robust fit, rename column so it works with fit
+# scramble$expn_med_fitted <- predict(wt_robust_fit, 
+#                                     select(scramble, expn_med_scramble = expn_med))
+# peak_tile$expn_med_fitted <- predict(wt_robust_fit,
+#                                      select(peak_tile, expn_med_scramble = expn_med))
+# flp3$expn_med_fitted <- predict(wt_robust_fit,
+#                                      select(flp3, expn_med_scramble = expn_med))
+# rlp6$expn_med_fitted <- predict(wt_robust_fit,
+#                                      select(rlp6, expn_med_scramble = expn_med))
 
-wt_robust_fit <- MASS::rlm(expn_med_tss ~ expn_med_scramble, wt_scramble)
+standardize <- function(df1, df2) {
+    # subset positive controls
+    pos_controls <- inner_join(filter(df1, category == 'pos_control') %>% 
+                                   select(name, expn_med1 = expn_med),
+                               filter(df2, category == 'pos_control') %>% 
+                                   select(name, expn_med2 = expn_med),
+                               by = 'name')
+    control_fit <- MASS::rlm(expn_med1 ~ expn_med2, pos_controls)
+    predicted <- predict(control_fit, select(df2, expn_med2 = expn_med))
+    return(predicted)
+}
 
-# fit expression using robust fit, rename column so it works with fit
-scramble$expn_med_fitted <- predict(wt_robust_fit, 
-                                    select(scramble, expn_med_scramble = expn_med))
-peak_tile$expn_med_fitted <- predict(wt_robust_fit,
-                                     select(peak_tile, expn_med_scramble = expn_med))
-flp3$expn_med_fitted <- predict(wt_robust_fit,
-                                     select(flp3, expn_med_scramble = expn_med))
-rlp6$expn_med_fitted <- predict(wt_robust_fit,
-                                     select(rlp6, expn_med_scramble = expn_med))
+scramble$expn_med_fitted <- standardize(tss, scramble)
+peak_tile$expn_med_fitted <- standardize(tss, peak_tile)
+flp3$expn_med_fitted <- standardize(tss, flp3)
+rlp6$expn_med_fitted <- standardize(tss, rlp6)
 
 # we do not need to scale TSS because it's the standard, but create a "fitted"
 # column for consistency
