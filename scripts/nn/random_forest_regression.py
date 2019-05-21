6b2ff9d4e12ec41bed3a817000d861eb965f689c
kinsigne <kiminsigne@gmail.com>
2019-05-21 16:05:42 -0700

    modified random forest to accept k-mer counts instead of raw sequence

--- scripts/nn/random_forest_regression.py
+++ scripts/nn/random_forest_regression.py
@@ -18,70 +18,18 @@ from collections import Counter
 num_epochs = 100
 
 
-def one_hot_encode(sequences):
-	# horizontal one-hot encoding
-    sequence_length = len(sequences[0])
-    integer_type = np.int8 if sys.version_info[
-        0] == 2 else np.int32  # depends on Python version
-    integer_array = LabelEncoder().fit(np.array(('ACGTN',)).view(integer_type)).transform(
-        sequences.view(integer_type)).reshape(len(sequences), sequence_length)
-    one_hot_encoding = OneHotEncoder(
-        sparse=False, n_values=5, dtype=integer_type).fit_transform(integer_array)
 
-    return one_hot_encoding.reshape(
-        len(sequences), 1, sequence_length, 5).swapaxes(2, 3)[:, :, [0, 1, 2, 4], :]
+def process_seqs(filename):
 
-
-def one_hot_encode_2d(sequences):
-	# horizontal one-hot encoding
-    sequence_length = len(sequences[0])
-    integer_type = np.int8 if sys.version_info[
-        0] == 2 else np.int32  # depends on Python version
-    integer_array = LabelEncoder().fit(np.array(('ACGTN',)).view(integer_type)).transform(
-        sequences.view(integer_type)).reshape(len(sequences), sequence_length)
-    one_hot_encoding = OneHotEncoder(
-        sparse=False, n_values=5, dtype=integer_type).fit_transform(integer_array)
-    # dimensions are n-samples, n-features. The one hot encoded vector is kept as a single
-    # vector instead of split into 1x4 matrix. n-features = 4 * sequence_length
-    return one_hot_encoding
-
-
-def pad_sequence(seq, max_length):
-	if len(seq) > max_length:
-		diff = len(seq) - max_length
-		# diff%2 returns 1 if odd
-		trim_length = int(diff / 2)
-		seq = seq[trim_length : -(trim_length + diff%2)]
-	else:
-		seq = seq.center(max_length, 'N')
-
-	return seq
-
-
-def process_seqs(filename, seq_length, encode_type='1d'):
-
-	seqs = [line.split('\t')[0] for line in open(filename)]
-	padded_seqs = [pad_sequence(x, seq_length) for x in seqs]
-	if encode_type == '1d':
-		X = one_hot_encode(np.array(padded_seqs))
-	elif encode_type == '2d':
-		# only keep 150bp sequences
-		X = one_hot_encode_2d(np.array([x for x in padded_seqs]))
-	else:
-		raise ValueException('Non-valid encoding type')
-
-	y = np.array([float(line.strip().split('\t')[1]) for line in open(filename)])
+	data = pd.read_table(filename, header=None)
+	sequences = data.loc[:, 0]
+	y = np.array(data.loc[:, 1])
+	X = np.array(data.loc[:, 2:])
+	
 	return X, y
 
 
-def get_kmers(seqs, k, cutoff):
 
-	kmers = list(itertools.chain(*[[x[i:i+k] for i in range(len(x)-k+1)] for x in seqs]))
-	kmer_count = Counter(kmers)
-	print "Unique k-mers:", len(kmer_count)
-	kmer_filtered = [x for x in kmer_count if kmer_count[x] >= cutoff]
-	print "k-mers above cutoff:", len(kmer_filtered)
-	return kmer_filtered
 
 
 if __name__ == '__main__':
@@ -91,20 +39,21 @@ if __name__ == '__main__':
 		one column expression. Tab-separated''')
 	parser.add_argument('test', help='''Test sest, one column sequence, 
 		one column expression. Tab-separated''')
-	parser.add_argument('seq_length', type=int, help='length of input sequences')
+	parser.add_argument('output_name')
 	args = parser.parse_args()
 
 	# load in pre-defined splits
 	seq_length = args.seq_length
 	print("loading training and test set...")
-	X_train, y_train = process_seqs(args.train, seq_length, '2d')
-	X_test, y_test = process_seqs(args.test, seq_length, '2d')
+	X_train, y_train = process_seqs(args.train)
+	X_test, y_test = process_seqs(args.test)
 
+	print("Running random forest regression...")
 	model = RandomForestRegression()
 	model.train(X_train, y_train)
 	predictions = model.predict(X_test)
 
-	with open('random_forest_regression_predictions.txt', 'w') as outfile:
+	with open(args.output_name, 'w') as outfile:
 		for i in range(len(predictions)):
 			outfile.write(str(float(predictions[i])) + '\t' +
 				      str(float(y_test[i])) + '\n')
