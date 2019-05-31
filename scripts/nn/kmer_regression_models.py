23781836c6eb692cee89479015cc19e93b46ffbe
Kimberly <kiminsigne@gmail.com>
2019-05-31 16:19:14 -0700

    kmer predictions

--- scripts/nn/kmer_regression_models.py
+++ scripts/nn/kmer_regression_models.py
@@ -17,25 +17,30 @@ if __name__ == '__main__':
     parser.add_argument('output_name', help='Output filename for predictions')
     parser.add_argument('method', help='regression method. linear, pls, svmlinear, svmradial')
 
+    args = parser.parse_args()
     train_name = args.train
     test_name = args.test
     output_name = args.output_name
     method = args.method
     
+    print "Reading training set..."
     X_train = pd.read_table(train_name)
     y_train = X_train['y']
     X_train.drop('y', axis=1, inplace=True)
 
+    print "Reading test set..."
     X_test = pd.read_table(test_name)
-    y_test = X_train['y']
+    y_test = X_test['y']
     X_test.drop('y', axis=1, inplace=True)
 
+    print "Fitting model..."
     if method == 'linear':
     	model = sm.OLS(y_train, X_train).fit()
     elif method == 'pls':
     	model = PLSRegression(n_components=2)
     	model.fit(X_train, y_train)
 
+    print "Predicting..."
     predictions = model.predict(X_test)
     # check if list needs to be flattened
     if type(predictions[0]) != np.float64:
@@ -43,6 +48,7 @@ if __name__ == '__main__':
     score = np.corrcoef(y_test, predictions)[0, 1]
     print("Correlation:", score)
 
+    print "Writing..."
     with open(output_name, 'w') as outfile:
     	for i in range(len(predictions)):
     		outfile.write(str(predictions[i]) + '\t' + str(y_test[i]) + '\n')
