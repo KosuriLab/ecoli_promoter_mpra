21242f1da86700f129bd1dc7cd4cabc796094d4f
Kimberly <kiminsigne@gmail.com>
2019-06-05 14:07:05 -0700

    gapped kmer svm test results

--- scripts/endo_tss/run_gkmsvm_tss.R
+++ scripts/endo_tss/run_gkmsvm_tss.R
@@ -31,27 +31,38 @@ write.fasta(as.list(train_positive$sequence), names = train_positive$name, nbcha
 write.fasta(as.list(train_negative$sequence), names = train_negative$name, nbchar = 300,
             file.out = '../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_negatives.fasta')
 
+kmer_length = 10
+ungapped <- 8
+# print("Creating kernel...")
+# gkmsvm_kernel(posfile = "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_positives.fasta", 
+#               negfile = "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_negatives.fasta", 
+#               outfile = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel', 
+#               L = kmer_length, K = ungapped)
+# 
+# print("Training...")
+# # perform SVM training with cross-validation
+# result <- gkmsvm_trainCV(kernelfn = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel',
+#                          posfn= "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_positives.fasta", 
+#                          negfn = "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_negatives.fasta",
+#                          svmfnprfx='../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel', 
+#                          outputCVpredfn='../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_cvpred',
+#                          outputROCfn='../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_roc', 
+#                          L = kmer_length, K = ungapped, showPlots = T, 
+#                          outputPDFfn = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_curves')
+# 
+# ggsave('../../processed_data/endo_tss/lb/model_files/gkmsvm_s10mer_8ungapped_ROC_PR_curves.png')
 
-gkmsvm_kernel(posfile = "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_positives.fasta", 
-              negfile = "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_negatives.fasta", 
-              outfile = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel', 
-              L = 10, K = 8)
+print("Classifying test set...")
+# read in test set and convert to FASTA
+test <- read.table('../../processed_data/endo_tss/lb/model_files/tss_expression_model_format_test_genome_split.txt',
+                   header = F, sep = '\t', col.names = c('sequence', 'expression'))
+test$name <- paste0('seq', seq(1:nrow(test)))
+write.fasta(as.list(test$sequence), names = test$name, nbchar=300,
+            file.out = '../../processed_data/endo_tss/lb/model_files/tss_expression_model_format_test_genome_split.fasta')
 
-# perform SVM training with cross-validation
-result <- gkmsvm_trainCV(kernelfn = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel',
-                         posfn= "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_positives.fasta", 
-                         negfn = "../../processed_data/endo_tss/lb/model_files/tss_train_genome_split_negatives.fasta",
-                         svmfnprfx='.../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernell', 
-                         outputCVpredfn='../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_cvpred',
-                         outputROCfn='../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_roc', 
-                         L = 10, K = 8, showPlots = T, 
-                         outputPDFfn = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_curves')
-
-ggsave('../../processed_data/endo_tss/lb/model_files/gkmsvm_s10mer_8ungapped_ROC_PR_curves.png')
+gkmsvm_classify(seqfile = '../../processed_data/endo_tss/lb/model_files/tss_expression_model_format_test_genome_split.fasta',
+                svmfnprfx = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel',
+                outfile = '../../processed_data/endo_tss/lb/model_files/gkmsvm_10mer_8ungapped_kernel_test_results',
+                L = kmer_length, K = ungapped)
 
 
-# # score all n-mers
-# gkmsvm_classify(seqfile = '../processed_data/gkmsvm_results/all_10mers.fasta',
-#                 svmfnprfx = '../processed_data/gkmsvm_results/promoter_svm_10mer_8ungapped_top10pct_300bp.out',
-#                 outfile = '../processed_data/gkmsvm_results/all_10mers_classified_12mer_8ungapped_top10pct_300bp.txt',
-#                 L = 10, K = 8)
