cc38ae9941ff18e481df614634470126121f27f5
kinsigne <kiminsigne@gmail.com>
2019-06-06 15:22:47 -0700

    script to run nn

---
+++ scripts/nn/run_nn.sh
@@ -0,0 +1,20 @@
+model_type=$1
+DATE=`date +%Y%m%d`
+
+if [ $model_type == 'regression' ]; then
+	python dragonn_hyperparameter_tuning_regression.py \
+	../../processed_data/combined/tss_scramble_peak_expression_model_format_floored_train_genome_split.txt \
+	 ../../processed_data/combined/tss_scramble_peak_expression_model_format_floored_test_genome_split.txt \
+	 150 4 5 100 0.2 100 \
+	 ../../processed_data/combined/${DATE}_tss_scramble_peak_regression_hyperparam_tuned > \
+	 ../../processed_data/combined/${DATE}_tss_scramble_peak_regression_hyperparam_tuned.log
+fi
+
+if [ $model_type == 'classification' ]; then
+	python dragonn_hyperparameter_tuning_regression.py \
+	../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_positives.fasta \
+	 ../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_negatives.fasta \
+	 150 4 5 100 0.2 100 \
+	 ../../processed_data/combined/${DATE}_tss_scramble_peak_classification_hyperparam_tuned > \
+	 ../../processed_data/combined/${DATE}_tss_scramble_peak_classification_hyperparam_tuned.log
+fi
