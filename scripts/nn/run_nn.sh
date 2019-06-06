5f48ff3e8b82adc1294ba3d21ffa4e3568428b57
kinsigne <kiminsigne@gmail.com>
2019-06-06 15:45:58 -0700

    modified arguments

--- scripts/nn/run_nn.sh
+++ scripts/nn/run_nn.sh
@@ -4,17 +4,19 @@ DATE=`date +%Y%m%d`
 if [ $model_type == 'regression' ]; then
 	python dragonn_hyperparameter_tuning_regression.py \
 	../../processed_data/combined/tss_scramble_peak_expression_model_format_floored_train_genome_split.txt \
-	 ../../processed_data/combined/tss_scramble_peak_expression_model_format_floored_test_genome_split.txt \
-	 150 4 5 100 0.2 100 \
-	 ../../processed_data/combined/${DATE}_tss_scramble_peak_regression_hyperparam_tuned > \
-	 ../../processed_data/combined/${DATE}_tss_scramble_peak_regression_hyperparam_tuned.log
+	../../processed_data/combined/tss_scramble_peak_expression_model_format_floored_test_genome_split.txt \
+	150 4 5 100 0.2 100 \
+	../../processed_data/combined/${DATE}_tss_scramble_peak_regression_hyperparam_tuned > \
+	../../processed_data/combined/${DATE}_tss_scramble_peak_regression_hyperparam_tuned.log
 fi
 
 if [ $model_type == 'classification' ]; then
 	python dragonn_hyperparameter_tuning.py \
 	../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_positives.fasta \
-	 ../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_negatives.fasta \
-	 150 4 5 100 0.2 100 \
-	 ../../processed_data/combined/${DATE}_tss_scramble_peak_classification_hyperparam_tuned > \
-	 ../../processed_data/combined/${DATE}_tss_scramble_peak_classification_hyperparam_tuned.log
+	../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_negatives.fasta \
+	../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_positives.fasta \
+	../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_negatives.fasta \
+	150 4 5 100 0.2 100 \
+	../../processed_data/combined/${DATE}_tss_scramble_peak_classification_hyperparam_tuned > \
+	../../processed_data/combined/${DATE}_tss_scramble_peak_classification_hyperparam_tuned.log
 fi
