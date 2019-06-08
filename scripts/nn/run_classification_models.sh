67506e4ea423599129be9b7b392f77dd1e71bdaa
kinsigne <kiminsigne@gmail.com>
2019-06-08 11:27:42 -0700

    kmer count script modified for both classifcation and regression files

--- scripts/nn/run_classification_models.sh
+++ scripts/nn/run_classification_models.sh
@@ -6,17 +6,10 @@
 
 # create k-mer counts
 Rscript create_kmer_counts.R \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_positives.fasta \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_positives_kmer_counts.txt
+../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_classification.txt \
+../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_classification_kmer_counts.txt
 
 Rscript create_kmer_counts.R \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_negatives.fasta \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_train_genome_split_negatives_kmer_counts.txt
+../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_classification.txt \
+../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_classication_kmer_counts.txt
 
-Rscript create_kmer_counts.R \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_positives.fasta \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_positives_kmer_counts.txt
-
-Rscript create_kmer_counts.R \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_negatives.fasta \
-../../processed_data/combined/tss_scramble_peak_expression_model_format_test_genome_split_negatives_kmer_counts.txt
